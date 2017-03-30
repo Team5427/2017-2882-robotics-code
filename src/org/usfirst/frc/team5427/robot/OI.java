976cc5bbfe00371a117146f8c5e597af3df70dde
Varsha Kumar <varsha.kumar@outlook.com>
2017-03-29 19:36:45 -0500

    need to fix errors

--- src/org/usfirst/frc/team5427/robot/OI.java
+++ src/org/usfirst/frc/team5427/robot/OI.java
@@ -1,12 +1,12 @@
 package org.usfirst.frc.team5427.robot;
 
+import org.usfirst.frc.team5427.robot.util.Config;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import org.usfirst.frc.team5427.robot.util.Config;
 
 /**
  * This class is the glue that binds the controls on the physical operator
